254243a3deaac86bb5e4641c8c14327a365fe7c9
sfrenk <stephen.frenk@gmail.com>
2018-02-20 13:48:59 -0500

    added compatability for gatk pipeline

--- snakemake/setup_dir.sh
+++ snakemake/setup_dir.sh
@@ -5,7 +5,7 @@
 # Directory containing Snakemake and cluster.json files
 snakedir='/nas/longleaf/home/sfrenk/pipelines/snakemake'
 
-usage="Create directory with Snakemake files required for pipeline \n\n setup_dir -p <pipeline> -d <directory> \n\n pipelines: srna_telo, bowtie_srna, hisat2_stringtie"
+usage="Create directory with Snakemake files required for pipeline \n\n setup_dir -p <pipeline> -d <directory> \n\n pipelines: srna_telo, bowtie_srna, hisat2_stringtie, chip_seq, gatk"
 
 pipeline=""
 
@@ -27,7 +27,7 @@ do
         shift
         ;;
         -h|--help)
-		echo "$usage"
+		printf "$usage"
 		exit
 		;;
     esac
@@ -36,19 +36,19 @@ done
 
 
 if [[ ! -d $dir ]]; then
-	echo "ERROR: invalid directory"
+	echo "ERROR: Invalid directory"
 	exit 1
 fi
 
 if [[ $pipeline == "" ]]; then
-	echo "ERROR: invalid pipeline"
+	echo "ERROR: Please select pipeline"
 	exit 1
 fi
 
 # Determine pipeline file
 case $pipeline in
 	"srna_telo")
-	snakefile="srna_telo"
+	snakefile="srna_telo.Snakefile"
 	modules="anaconda python bowtie/1.1.2 samtools subread"
 	;;
 	"bowtie_srna")
@@ -58,6 +58,18 @@ case $pipeline in
 	"hisat2_stringtie")
 	snakefile='hisat2_stringtie.Snakefile'
 	modules="python bbmap hisat2 samtools subread"
+	;;
+	"chip_seq")
+	snakefile='chip_seq.Snakefile'
+	modules="anaconda python bbmap bowtie/1.1.2 samtools"
+	;;
+	"gatk")
+	snakefile='call_variants.Snakefile'
+	modules="python bbmap bwa samtools gatk picard"
+	;;
+	*)
+	echo "ERROR: Invalid pipeline. Please select one of the following: bowtie_srna, hisat2_stringtie, srna_telo, chip_seq"
+	exit 1
 	;;
 esac
 
@@ -71,17 +83,21 @@ sed -r -i -e "s,^BASEDIR.*,BASEDIR = \"${dir}\"," "$snakefile"
 # Determine file extension
 extension="$(ls $dir | grep -Eo "\.[^/]+" | sort | uniq)"
 
-if [[ "${#extension[@]}" != 1 ]]; then
-	echo "ERROR: All files must have the same extension"
+# Check if there are multiple file extensions in the same directory
+ext_count="$(echo $extension | wc -l)"
 
-else
-
-	# Edit extension in Snakefile
-	extension="\"${extension}\""
-	sed -i -r -e "s/^EXTENSION.*/EXTENSION = ${extension}/g" "$snakefile"
+if [[ ext_count == 0 ]]; then
+	echo "ERROR: Directory is empty!"
+elif [[ ext_count != 1 ]]; then
+	echo "WARNING: Multiple file extensions found: using .fastq.gz"
+	extension=".fastq.gz"
 fi
 
-# Create Snakmake command script
+# Edit extension in Snakefile
+extension="\"${extension}\""
+sed -i -r -e "s/^EXTENSION.*/EXTENSION = ${extension}/g" "$snakefile"
 
-echo "module add $modules" > "run_snakemake.sh"
-echo "snakemake -s $snakefile --cluster-config ${snakedir}/cluster.json -j 100 --cluster \"sbatch -n {cluster.n} -N {cluster.N} -t {cluster.time}\"" >> "run_snakemake.sh"
+# Create Snakmake command script
+printf "#!/usr/bin/bash\n\n" > "run_snakemake.sh"
+printf "module add $modules\n\n" >> "run_snakemake.sh"
+printf "snakemake -s $snakefile --cluster-config ${snakedir}/cluster.json -j 100 --cluster \"sbatch -n {cluster.n} -N {cluster.N} -t {cluster.time}\"\n" >> "run_snakemake.sh"
